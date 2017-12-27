e1f2d00203e6c57ee9ec437b310664eb7b76b62e
danielchan <danielchan@advantech.com.tw>
2017-12-27 17:45:57 +0800

    modify emmc-download log.

--- recipes-bsp/advantech-emmc-download/advantech-emmc-download/emmc-download.sh
+++ recipes-bsp/advantech-emmc-download/advantech-emmc-download/emmc-download.sh
@@ -19,19 +19,24 @@ ROOTFS_FILE=adv-image-qt5-imx6dl-dmsse23.ext4
 #   rm ${TMP_FOLDER} -rf
 #}
 
+
+printlog(){
+    echo "$1" | tee /dev/tty0
+}
+
 bootloader(){
-    echo -e "[EMMC-Download]: Downloading booloader...\c" | tee /dev/kmsg | tee /dev/tty0
+    printlog "[EMMC-Download]: Downloading booloader..."
     retval=1
     BOOTLOADER_PATH=${SOURCE_DIR}/${BOOTLOADER_FILE}
     SPI_ROM=/dev/mtdblock0
 
     if [ ! -e ${BOOTLOADER_PATH} ]; then
-        echo "[EMMC-Download]: Bootloader file not found!" | tee /dev/kmsg | tee /dev/tty0
+        printlog "[EMMC-Download]: Bootloader file not found!"
         retval=1
     fi
 
     if [ ! -e ${SPI_ROM} ]; then
-        echo "[EMMC-Download]: SPI_ROM device not found!" | tee /dev/kmsg | tee /dev/tty0
+        printlog "[EMMC-Download]: SPI_ROM device not found!"
         retval=1
     else
         retval=0
@@ -42,9 +47,9 @@ bootloader(){
         do
             dd if=${BOOTLOADER_PATH} of=${SPI_ROM} bs=512 seek=2
             if [ $? -ne 0 ]; then
-                echo "[EMMC-Download]: dd failed\n" | tee /dev/kmsg | tee /dev/tty0
+                printlog "[EMMC-Download]: dd failed\n"
             else
-                echo "[EMMC-Download]: u-boot download OK." | tee /dev/kmsg | tee /dev/tty0
+                printlog "[EMMC-Download]: u-boot download OK."
 
                 retval=0
                 break
@@ -63,19 +68,19 @@ bootloader(){
 }
 
 bootimage(){
-    echo -e "[EMMC-Download]: Coping boot files...\c" | tee /dev/kmsg | tee /dev/tty0
+    printlog  "[EMMC-Download]: Coping boot files..."
     TARGET_DIR=p1
     retval=1
     
     if [ ! -e ${EMMC_BOOT_PATH} ]; then
-        echo "[EMMC-Download]:Bootloader file not found!" | tee /dev/kmsg | tee /dev/tty0
+        printlog "[EMMC-Download]:Bootloader file not found!"
         return 1
     fi
     
     mkdir ${TARGET_DIR}
     mount -t vfat ${EMMC_BOOT_PATH} ${TARGET_DIR}
     if [ $? -ne 0 ]; then
-        echo "[EMMC-Download]: Fail to mount emmc boot partition!" | tee /dev/kmsg | tee /dev/tty0
+        printlog "[EMMC-Download]: Fail to mount emmc boot partition!"
         return 1
     fi
     
@@ -83,9 +88,9 @@ bootimage(){
     do
         cp ${SOURCE_DIR}/${DTB_FILE} ${TARGET_DIR}/
         if [ $? -ne 0 ] || [ ! -e "${TARGET_DIR}/${DTB_FILE}" ]; then
-            echo -e "[EMMC-Download]: Copy boot dtb file failed\c" | tee /dev/kmsg | tee /dev/tty0
+            printlog "[EMMC-Download]: Copy boot dtb file failed"
         else
-            echo -e "[EMMC-Download]: copy dtb OK.\c" | tee /dev/kmsg | tee /dev/tty0
+            printlog "[EMMC-Download]: Copy dtb OK."
             retval=0
             break
         fi
@@ -101,9 +106,9 @@ bootimage(){
     do
         cp ${SOURCE_DIR}/${BOOT_FILE} ${TARGET_DIR}/
         if [ $? -ne 0 ] || [ ! -e "${TARGET_DIR}/${BOOT_FILE}" ]; then
-            echo "[EMMC-Download]: Copy boot file failed" | tee /dev/kmsg | tee /dev/tty0
+            printlog "[EMMC-Download]: Copy boot file failed"
         else
-            echo "[EMMC-Download]: copy boot file OK." | tee /dev/kmsg | tee /dev/tty0
+            printlog "[EMMC-Download]: Copy boot file OK."
             retval=0
             break
         fi
@@ -119,17 +124,17 @@ bootimage(){
 }
 
 rootfs(){
-    echo "[EMMC-Download]: Copy bootfs..." | tee /dev/kmsg | tee /dev/tty0
+    printlog "[EMMC-Download]: Create rootfs..."
     TARGET_PATH=p2
     SOURCE_PATH=s2
     
     retval=1
-	
-	if [ -e ${TARGET_PATH} ]; then
+
+    if [ -e ${TARGET_PATH} ]; then
         rm -rf ${TARGET_PATH}
     fi
-	
-	if [ -e ${SOURCE_PATH} ]; then
+
+    if [ -e ${SOURCE_PATH} ]; then
         rm -rf ${SOURCE_PATH}
     fi
     
@@ -137,32 +142,38 @@ rootfs(){
     mkdir ${SOURCE_PATH}
     
     if [ ! -e ${EMMC_ROOTFS_PATH} ] || [ ! -e ${TARGET_PATH} ]; then
-        echo "[EMMC-Download]: File not found:"${EMMC_ROOTFS_PATH} | tee /dev/kmsg | tee /dev/tty0
+        printlog "[EMMC-Download]: File not found:"${EMMC_ROOTFS_PATH}
         return 1
     fi
-    
+
+    printlog "[EMMC-Download]: Mount rootfs partition..."
     mount -t ext4 ${EMMC_ROOTFS_PATH} ${TARGET_PATH}
     if [ $? -ne 0 ]; then
-        echo "[EMMC-Download]: EMMC partition mount failed" | tee /dev/kmsg | tee /dev/tty0
+        printlog "[EMMC-Download]: rootfs partition mount failed"
         return 1
     fi
     
+    printlog "[EMMC-Download]: Mount rootfs file..."
     mount ${SOURCE_DIR}/${ROOTFS_FILE} ${SOURCE_PATH}
     if [ $? -ne 0 ]; then
         umount ${TARGET_PATH}
-        echo "[EMMC-Download]: Rootfs file mount failed" | tee /dev/kmsg | tee /dev/tty0
+        printlog "[EMMC-Download]: rootfs file mount failed"
         return 1
     fi
     
-    
+    printlog "[EMMC-Download]: Copy rootfs..."
     for i in ${TRY_ERROR}
     do
         cp -R ${SOURCE_PATH}/* ${TARGET_PATH}/
         if [ $? -ne 0 ]; then
-            echo "[EMMC-Download]: Copy rootfs file failed\n" | tee /dev/kmsg | tee /dev/tty0
+            printlog "[EMMC-Download]: Copy rootfs file failed"
+            printlog "[EMMC-Download]: Please check SD image!"
+            read -rsp $'press any key...'
         else
-            echo "[EMMC-Download]: Copy rootfs ok!" | tee /dev/kmsg | tee /dev/tty0
-			echo "[EMMC-Download]: Done and Success!" | tee /dev/kmsg | tee /dev/tty0
+            printlog "[EMMC-Download]: Copy rootfs ok!"
+            printlog "[EMMC-Download]: Done and Success!"
+            printlog "[EMMC-Download]: Please remove SD Card and reboot device!"
+            read -rsp $'press any key...'
             retval=0
             break
         fi
@@ -177,22 +188,22 @@ rootfs(){
 
 validateSourceFile(){
     if [ ! -e ${SOURCE_DIR}/${BOOTLOADER_FILE} ]; then
-        echo "EMMC-Download]: Bootloader file: "${SOURCE_DIR}/${BOOTLOADER_FILE}" not found!" | tee /dev/kmsg | tee /dev/tty0
+        printlog "EMMC-Download]: Bootloader file: "${SOURCE_DIR}/${BOOTLOADER_FILE}" not found!"
         return 1
     fi
     
     if [ ! -e ${SOURCE_DIR}/${DTB_FILE} ]; then
-        echo "EMMC-Download]: DTB file not found!" | tee /dev/kmsg | tee /dev/tty0
+        printlog "EMMC-Download]: DTB file not found!"
         return 1
     fi
     
     if [ ! -e ${SOURCE_DIR}/${BOOT_FILE} ]; then
-        echo "EMMC-Download]: Boot file not found!" | tee /dev/kmsg | tee /dev/tty0
+        printlog "EMMC-Download]: Boot file not found!"
         return 1
     fi
     
     if [ ! -e ${SOURCE_DIR}/${ROOTFS_FILE} ]; then
-        echo "EMMC-Download: Rootfs file not found!" | tee /dev/kmsg | tee /dev/tty0
+        printlog "EMMC-Download: Rootfs file not found!"
         return 1
     fi
     return 0;
@@ -200,13 +211,16 @@ validateSourceFile(){
 
 main(){
     # Preparing for backup block
-
-    echo "[EMMC-Download]: Preparing for backup block" | tee /dev/kmsg | tee /dev/tty0
+    echo "" | tee /dev/tty0
+    echo "" | tee /dev/tty0
+    printlog "[EMMC-Download]: Preparing for backup block"
 
     
     validateSourceFile
     if [ $? -ne 0 ]; then
         #clean
+        printlog "[EMMC-Download]: Please check SD image!"
+		read -rsp $'press any key...'
         return 1
     fi
 
@@ -214,29 +228,33 @@ main(){
     bootloader
     retval=$?
     if [ ${retval} -ne 0 ]; then
+        printlog "[EMMC-Download]: Please check SD image!"
+        read -rsp $'press any key...'
         return 1
     fi
     
     # Do file system
 
-    echo "[EMMC-Download]: Do EMMC file system..." | tee /dev/kmsg | tee /dev/tty0
-	dd if=/dev/zero of=${EMMC_PATH}  bs=1M  count=10
-	
-    echo "[Do EMMC file system]..." | tee /dev/kmsg | tee /dev/tty0
+    printlog "[EMMC-Download]: Do EMMC file system..."
+    dd if=/dev/zero of=${EMMC_PATH}  bs=1M  count=10
+
+    printlog "[EMMC-Download]: Format EMMC..."
     echo -e "n\np\n1\n2048\n260096\nn\np\n2\n\n\nw" | fdisk ${EMMC_PATH}
-    mkfs.vfat ${EMMC_BOOT_PATH}
-    mkfs.ext4  ${EMMC_ROOTFS_PATH}
+    mkfs.vfat  ${EMMC_BOOT_PATH}
+    mkfs.ext4 -F ${EMMC_ROOTFS_PATH}
     
     # Copy zImage dtb
     bootimage
-	
+
     if [ $? -ne 0 ]; then
+        printlog "[EMMC-Download]: Please check SD image!"
+        read -rsp $'press any key...'
         return 1
     fi
     
     # Copy rootfs
     rootfs
-	
+
 }
 
 main
