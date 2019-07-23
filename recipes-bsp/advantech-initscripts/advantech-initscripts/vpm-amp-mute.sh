46c711c3403ef52d4bd941b8ac8605344854e672
jerryliao <jerry.liao@advantech.com.tw>
2019-07-23 11:07:23 +0800

    synchronize the audio amplifier initial sequence with VPM

--- recipes-bsp/advantech-initscripts/advantech-initscripts/vpm-amp-mute.sh
+++ recipes-bsp/advantech-initscripts/advantech-initscripts/vpm-amp-mute.sh
@@ -8,10 +8,18 @@
 # Default-Stop:
 ### END INIT INFO
 
+VPM_INIT_AMP_CHIP="/sys/devices/soc0/soc/2100000.aips-bus/21a0000.i2c/i2c-0/0-0068/vpm_amp_on"
 VPM_INIT_AMP_MODE="/sys/devices/soc0/soc/2100000.aips-bus/21a0000.i2c/i2c-0/0-0068/vpm_amp_mute"
+
+if [ -f "$VPM_INIT_AMP_CHIP" ]; then
+	echo 1 > "$VPM_INIT_AMP_CHIP"
+else
+	echo "sysfs entry for vpm amp chip is not exist"
+fi
 
 if [ -f "$VPM_INIT_AMP_MODE" ]; then
 	echo 0 > "$VPM_INIT_AMP_MODE" 
 else
 	echo "sysfs entry for vpm amp mute is not exist"
 fi
+
