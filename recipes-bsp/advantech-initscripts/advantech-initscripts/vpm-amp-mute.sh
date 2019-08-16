2b93be7e3776fced1b1bea96fe538884d5057bf9
jerryliao <jerry.liao@advantech.com.tw>
2019-08-16 15:55:44 +0800

    notify VPM to stop AMP mute service by operating GPIO_16

--- recipes-bsp/advantech-initscripts/advantech-initscripts/vpm-amp-mute.sh
+++ recipes-bsp/advantech-initscripts/advantech-initscripts/vpm-amp-mute.sh
@@ -11,6 +11,25 @@
 VPM_INIT_AMP_CHIP="/sys/devices/soc0/soc/2100000.aips-bus/21a0000.i2c/i2c-0/0-0068/vpm_amp_on"
 VPM_INIT_AMP_MODE="/sys/devices/soc0/soc/2100000.aips-bus/21a0000.i2c/i2c-0/0-0068/vpm_amp_mute"
 
+SYSFS_GPIO="/sys/class/gpio"
+VPM_NOTIFY_GPIO="16"
+VPM_NOTIFY_GPIO_SYSFS="${SYSFS_GPIO}/gpio${VPM_NOTIFY_GPIO}"
+
+
+echo "${VPM_NOTIFY_GPIO}" > "${SYSFS_GPIO}/export"
+sleep 1
+
+if [ -d "${VPM_NOTIFY_GPIO_SYSFS}" ]; then
+	echo "notify vpm to stop amp mute"
+	
+	echo "out" > "${VPM_NOTIFY_GPIO_SYSFS}/direction"
+	echo 0 > "${VPM_NOTIFY_GPIO_SYSFS}/value"
+	sleep 1
+else
+
+	echo "sysfs entry for vpm stop amp mute is not exist"
+fi
+
 if [ -f "$VPM_INIT_AMP_CHIP" ]; then
 	echo 1 > "$VPM_INIT_AMP_CHIP"
 else
